82a438df7eede8f4011edfaa61ff9601c5c62217
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-09 23:32:24 -0600

    Update multiple_gan.py

--- multiple_gan.py
+++ multiple_gan.py
@@ -132,10 +132,10 @@ print("Using: " + str(device))
 # Hyperparameters
 z_dim = 20
 z_dim_2 = 20
-learning_rate_gen = 0.000001
-learning_rate_gen_2 = 0.000002
-learning_rate_disc = 0.00000001
-learning_rate_disc_2 = 0.0000002
+learning_rate_gen = 0.00000001
+learning_rate_gen_2 = 0.00000002
+learning_rate_disc = 0.0000000001
+learning_rate_disc_2 = 0.000000002
 batch_size = 100
 img_channels = 1
 img_size = 256
