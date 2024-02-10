e02c75175641a457ab8d60add43d39de0c9792d4
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-09 18:11:04 -0600

    Update multiple_gan.py

--- multiple_gan.py
+++ multiple_gan.py
@@ -298,6 +298,26 @@ for epoch in range(num_epochs):
 
         print(f"Epoch {epoch+1} Sub: {batch_idx} Loss D: {loss_disc:.4f}, Loss G: {total_gen_loss:.4f} Loss D2: {loss_disc_2:.4f}, Loss G2: {total_gen_loss_2:.4f}")
 
+        epsilon = 0.00001
+    
+        if(total_gen_loss <= epsilon):
+            generator.load_state_dict(torch.load(generator_2_path, map_location=device))
+            generator.to(device)
+            print("Generator model and was killed and revived as Generator_2")
+        if(total_gen_loss_2 <= epsilon):
+            generator_2.load_state_dict(torch.load(generator_path, map_location=device))
+            generator_2.to(device)
+            print("Generator_2 model and was killed and revived as Generator")
+                
+        if(loss_disc <= epsilon or loss_disc >= 50 - epsilon):
+            discriminator.load_state_dict(torch.load(discriminator_2_path, map_location=device))
+            discriminator.to(device)
+            print("Discriminator model and was killed and revived as Discriminator_2")
+        if(loss_disc_2 <= epsilon or loss_disc_2 >= 50 - epsilon):
+            discriminator_2.load_state_dict(torch.load(discriminator_path, map_location=device))
+            discriminator_2.to(device)
+            print("Discriminator_2 model and was killed and revived as Discriminator_2")
+
 
     if (epoch + 1) % 1 == 0:
         torch.save(generator.state_dict(), 'generator.pth')
@@ -306,25 +326,7 @@ for epoch in range(num_epochs):
         torch.save(discriminator_2.state_dict(), 'discriminator_2.pth')
         print("Models all saved")
     
-    epsilon = 0.00001
-
-    if(total_gen_loss <= epsilon):
-        generator.load_state_dict(torch.load(generator_2_path, map_location=device))
-        generator.to(device)
-        print("Generator model and was killed and revived as Generator_2")
-    if(total_gen_loss_2 <= epsilon):
-        generator_2.load_state_dict(torch.load(generator_path, map_location=device))
-        generator_2.to(device)
-        print("Generator_2 model and was killed and revived as Generator")
-            
-    if(loss_disc <= epsilon or loss_disc >= 50 - epsilon):
-        discriminator.load_state_dict(torch.load(discriminator_2_path, map_location=device))
-        discriminator.to(device)
-        print("Discriminator model and was killed and revived as Discriminator_2")
-    if(loss_disc_2 <= epsilon or loss_disc_2 >= 50 - epsilon):
-        discriminator_2.load_state_dict(torch.load(discriminator_path, map_location=device))
-        discriminator_2.to(device)
-        print("Discriminator_2 model and was killed and revived as Discriminator_2")
+    
         
         
     # Logging
