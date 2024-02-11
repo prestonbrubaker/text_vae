a619cfbe7880c0efe239085d8fabb42b6dfb383b
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-10 18:56:21 -0600

    Update gan.py

--- gan.py
+++ gan.py
@@ -24,19 +24,19 @@ class ConvGenerator(nn.Module):
             nn.BatchNorm2d(512),
             nn.ReLU(True),
             # State size: 512 x 4 x 4
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2, padding=1, bias=True),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
             # State size: 256 x 8 x 8
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2, padding=1, bias=True),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
             # State size: 128 x 16 x 16
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2, padding=1, bias=True),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
             # State size: 64 x 32 x 32
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2, padding=1, bias=True),
             nn.BatchNorm2d(32),
             nn.ReLU(True),
             # State size: 32 x 128 x 128 (previously was 32 x 64 x 64, adjusted the stride to 2 for correct upscaling)
