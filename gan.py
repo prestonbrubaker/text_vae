56731d9312d2a7bea59cc7caa1b071247b76134b
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-10 18:45:32 -0600

    Update gan.py

--- gan.py
+++ gan.py
@@ -14,38 +14,31 @@ torch.cuda.empty_cache()
 class Generator(nn.Module):
     def __init__(self, z_dim, img_channels, img_size):
         super(Generator, self).__init__()
-        # Initial size for the feature maps
-        self.init_size = img_size // 8  # Assuming img_size is a power of 2 and at least 64
-        self.z_dim = z_dim
-        self.img_channels = img_channels
-        
-        self.gen = nn.Sequential(
-            # Input is Z, going into a convolution
-            nn.ConvTranspose2d(z_dim, 512, kernel_size=4, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            # State size. 512 x 4 x 4
-            
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            # State size. 256 x 8 x 8
-            
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+        self.img_size = img_size
+        self.label_emb = nn.Embedding(10, z_dim)
+
+        self.init_size = img_size // 16  # This will map to 16x16
+        self.fc = nn.Linear(z_dim, 128 * self.init_size ** 2)
+
+        self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            # State size. 128 x 16 x 16
-            
-            nn.ConvTranspose2d(128, img_channels, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.Tanh()
-            # Output state size. img_channels x 32 x 32
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, img_channels, 3, stride=1, padding=1),
+            nn.Tanh(),
         )
 
-    def forward(self, noise):
-        # Assuming noise is of shape (batch_size, z_dim)
-        noise = noise.view(-1, self.z_dim, 1, 1)  # Reshape to (batch_size, z_dim, 1, 1)
-        return self.gen(noise)
-
+    def forward(self, z):
+        out = self.fc(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
 
 class Discriminator(nn.Module):
     def __init__(self, img_channels=1):
