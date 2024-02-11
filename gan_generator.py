f672ec9ad708fad6dcd89c01eea8158a45fbbe21
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-11 10:14:59 -0600

    Update gan_generator.py

--- gan_generator.py
+++ gan_generator.py
@@ -7,35 +7,28 @@ import torch.nn.functional as F
 
 
 class Generator(nn.Module):
-    def __init__(self, z_dim, img_channels=1, img_size=256):
+    def __init__(self, z_dim=100, img_channels=1):
         super(Generator, self).__init__()
-        self.img_size = img_size
-        # Calculate the size of the tensor before the first ConvTranspose2d layer
-        self.init_size = img_size // 16
-        self.fc = nn.Linear(z_dim, 512 * self.init_size ** 2)
-
-        self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(512, 256, 4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(64, img_channels, 4, stride=2, padding=1, bias=False),
-            nn.Tanh()
+        self.gen = nn.Sequential(
+            # Input: Z_dim x 1 x 1
+            self._block(z_dim, 512, 4, 1, 0),  # img: 4x4
+            self._block(512, 256, 4, 2, 1),    # img: 8x8
+            self._block(256, 128, 4, 2, 1),    # img: 16x16
+            self._block(128, 64, 4, 2, 1),     # img: 32x32
+            nn.ConvTranspose2d(64, img_channels, 4, 2, 1),  # img: 64x64
+            nn.ConvTranspose2d(img_channels, img_channels, 16, 4, 6),  # img: 256x256
+            nn.Tanh()  # Output: img_channels x 256 x 256
         )
 
-    def forward(self, noise):
-        # Reshape noise to match the expected input for the first ConvTranspose2d layer
-        noise = self.fc(noise)
-        noise = noise.view(-1, 512, self.init_size, self.init_size)
-        img = self.conv_blocks(noise)
-        return img
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        return self.gen(x)
 
 
 
@@ -47,7 +40,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using: " + str(device))
 
 
-z_dim = 500
+z_dim = 100
 img_channels = 1
 img_size = 256
 generator = Generator(z_dim, img_channels).to(device)
